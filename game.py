f5dfbf7062aa53b49478c31b8b47bd762cc68124
Tavina Chen <tavinachen.work@gmail.com>
2026-04-21 14:09:19 -0700

    added alpha beta pruning minimax skeleton

--- game.py
+++ game.py
@@ -1,11 +1,14 @@
 import time
 
 gameIsDone = False
+board = [["-" for _ in range(8)] for _ in range(8)]
 
 def start_game(humanFirst:bool, thinkTimeInSeconds:int) -> None:
+    global currPlayer
     print("Game Started!")
     print_board()
     if humanFirst:
+        currPlayer = "O"
         human_move()
     
     while not gameIsDone:
@@ -15,17 +18,75 @@ def start_game(humanFirst:bool, thinkTimeInSeconds:int) -> None:
         human_move()
 
 def print_board() -> None:
-    pass
+    print("  1 2 3 4 5 6 7 8")
+    for i in range(len(board)):
+        print(chr(ord('A')+i), " ".join(board[i]))
 
 def ai_move(thinkTimeInSeconds: int):
     print("AI thinking...")
+
+    depth = 1
     start_time = time.time()
     while time.time() < start_time + thinkTimeInSeconds:
-        # insert algorithm here
-        pass
-    # make_move()
+        bestMove = alpha_beta_pruning(float('-inf'), float('inf'), depth)
+        depth +=1
+    make_move(bestMove[0], bestMove[1], "X")
+    print("AI chose: ", bestMove[0], bestMove[1])
     print_board()
     check_win_condition()
+
+def alpha_beta_pruning(a:int, b:int, maxDepth:int):
+    bestScore = a
+    for i in range(8):
+        for j in range(8):
+            if board[i][j] != "-":
+                continue
+            make_move(i, j, "X")
+            score = max(a, b, maxDepth-1)
+            if score > bestScore:
+                bestScore = score
+                bestMove = (i, j)
+            undo_move(i, j)
+    return bestMove
+
+def max(a:int, b:int, depth:int):
+    if depth == 0:
+        return eval_func()
+
+    bestScore = float('-inf')
+    for i in range(8):
+        for j in range(8):
+            if board[i][j] != "-":
+                continue
+            make_move(i, j, "X")
+            score = min(a, b, depth-1)
+            bestScore = max(score, bestScore)
+            if bestScore>= b:
+                return bestScore
+            a = max(a, bestScore)
+            undo_move(i, j)
+    return bestScore
+
+def min(a:int, b:int, depth:int):
+    if depth == 0:
+        return eval_func()
+    
+    bestScore = float('inf')
+    for i in range(8):
+        for j in range(8):
+            if board[i][j] != "-":
+                continue
+            make_move(i, j, "X")
+            score = max(a, b, depth-1)
+            bestScore = min(score, bestScore)
+            if bestScore <= b:
+                return bestScore
+            b = min(b, bestScore)
+            undo_move(i, j)
+    return bestScore
+
+def eval_func():
+    pass
 
 def check_win_condition():
     global gameIsDone
@@ -45,16 +106,18 @@ def human_move():
                 print("Move already taken!")
                 continue
 
-            make_move(row, col)
+            make_move(row, col, "O")
             print_board()
             check_win_condition()
             break
         else: 
             print("Invalid Move")
 
-def make_move(row:int, col:int):
-    pass
+def make_move(row:int, col:int, currPlayer):
+    board[row][col] = currPlayer
+
+def undo_move(row: int, col:int):
+    make_move(row, col, "-")
 
 def isMoveTaken(row:int, col:int) -> bool:
-    return False
-    pass
+    return board[row][col] != "-"
